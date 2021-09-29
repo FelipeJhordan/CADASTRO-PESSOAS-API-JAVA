d4bb295e9f62d2034b55866c144d39deb05ba546
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-09-29 10:29:11 -0300

    Project Inicializate

---
+++ src/test/java/com/felipe/criacaousuarios/CriacaoUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.criacaousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriacaoUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
