ba3d076517c291a6d92331aacd95c15fb9248035
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-09-29 17:15:08 -0300

    Create method updateById

--- src/main/java/com/felipe/criacaousuarios/service/PersonService.java
+++ src/main/java/com/felipe/criacaousuarios/service/PersonService.java
@@ -6,6 +6,7 @@ import com.felipe.criacaousuarios.entity.Person;
 import com.felipe.criacaousuarios.exception.PersonNotFoundException;
 import com.felipe.criacaousuarios.mapper.PersonMapper;
 import com.felipe.criacaousuarios.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +17,12 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
